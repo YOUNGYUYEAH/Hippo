8526c4e0198a8914ea748c12e29ad4210f1e1120
YOUNGYUYEAH <531144968@qq.com>
2019-03-15 18:29:29 +0800

    use cookie auto login

--- Hippo/settings.py
+++ Hippo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'HippoWeb'
 ]
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- HippoWeb/views.py
+++ HippoWeb/views.py
@@ -1,34 +1,45 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, render_to_response, redirect, HttpResponseRedirect
 from django.contrib import auth
 from HippoWeb.forms import *
 import rsa
 
+
+def rsa_encrypt(req):
+    """使用RSA生成公钥私钥"""
+    (public, private) = rsa.newkeys(512)
+    req.session['privkey'] = str(private)  # session存放在 django_session表内
+    pubkey = {'pub_e': hex(public.e), 'pub_n': hex(public.n)}  # hex转化为16进制数
+    return pubkey
+
 def login(req):
     """登录验证页"""
+    pubkey = rsa_encrypt(req)
+    pub_e = pubkey['pub_e']
+    pub_n = pubkey['pub_n']
     login_user = req.COOKIES.get('username')
     if login_user:
         return HttpResponseRedirect('/index/')
     else:
         login_form = LoginForm()
         if req.method == 'GET':
-            return render(req, 'login.html', {'login_form': login_form})
+            return render_to_response('login.html', {'login_form': login_form, 'pub_e': pub_e, 'pub_n': pub_n})
         elif req.method == 'POST':
             username = req.POST.get('username', None)
             password = req.POST.get('password', None)
             user = auth.authenticate(username=username, password=password)
             if user is not None and user.is_active:
-                req.session['IS_LGOIN'] = True
-                response = redirect('/index/')
-                response.set_cookie('username', username)
+                req.session['IS_LOGIN'] = True
                 auth.login(req, user)
+                response = render_to_response('index.html', {'username': username})
+                response.set_cookie('username', username)
                 return response
             else:
-                return render(req, 'login.html', {'login_form': login_form, 'passwd_wrong': True})
+                return render(req, 'login.html', {'login_form': login_form, 'password_wrong': True, 'pub_e': pub_e, 'pub_n': pub_n})
 
 
 def logout(req):
     """注销已登录用户"""
-    login_user =  req.COOKIES.get('username')
+    login_user = req.COOKIES.get('username')
     if login_user:
         response = redirect('/login/')
         response.delete_cookie('username')
