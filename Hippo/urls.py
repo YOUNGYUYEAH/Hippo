48774334ca1c0b3b516b3faa64e9b872ed41f6a9
YOUNGYUYEAH <531144968@qq.com>
2019-03-18 16:51:32 +0800

    use cookie auto login

--- Hippo/urls.py
+++ Hippo/urls.py
@@ -2,11 +2,8 @@ from django.conf.urls import url, include
 from django.conf import settings
 from django.contrib import admin
 from django.views.static import serve
-from HippoWeb.views import page_not_found, page_error
-
 admin.autodiscover()
-handler404 = page_not_found
-handler500 = page_error
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('HippoWeb.urls')),
