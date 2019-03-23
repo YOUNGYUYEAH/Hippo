be83714574527eac830efaeca4863c905d153545
YOUNGYUYEAH <531144968@qq.com>
2019-03-23 08:28:25 +0800

    20190323

--- HippoTools/main.py
+++ HippoTools/main.py
@@ -1,1 +1,15 @@
-import 
+# -*- encoding:utf-8 -*-
+import hippoagent
+import threading
+
+
+def fun_timer():
+    print(hippoagent.createjson())
+    global timer
+    timer = threading.Timer(60, fun_timer)
+    timer.start()
+
+
+if __name__ == '__main__':
+    timer = threading.Timer(1, fun_timer)
+    timer.start()
