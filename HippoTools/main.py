947df8eacfdedcb0fd66fe7bae08281b1334b595
YOUNGYUYEAH <531144968@qq.com>
2019-03-23 09:37:45 +0800

    20190323-02

--- HippoTools/main.py
+++ HippoTools/main.py
@@ -1,18 +1,17 @@
 # -*- encoding:utf-8 -*-
 import hippoagent
 import threading
-import sys
 
 
-def fun_timer(time=60):
+def fun_timer(t):
     """定时器执行监控"""
     print(hippoagent.createjson())
     global timer
-    timer = threading.Timer(time, fun_timer)
+    timer = threading.Timer(int(t), fun_timer(t))
     timer.start()
 
 
 if __name__ == '__main__':
-    option = sys.argv[1]
-    timer = threading.Timer(1, fun_timer(option))
+    time = 10
+    timer = threading.Timer(1, fun_timer(time))
     timer.start()
