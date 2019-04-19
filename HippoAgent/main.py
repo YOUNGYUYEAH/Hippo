03eb6b87bc798bbfe4dde12dcfa5eeb79c0295a7
YOUNGYUYEAH <531144968@qq.com>
2019-04-19 16:31:34 +0800

    cnm的bootstrap前端

--- HippoAgent/main.py
+++ HippoAgent/main.py
@@ -1,22 +1,27 @@
 # -*- encoding:utf-8 -*-
+import sys
 import threading
 import hippoagent
 import config
 
 
-def fun_timer():
+def subtimer():
     """定时器执行监控"""
-    # print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     try:
         hippoagent.pusher()
     except Exception as e:
         print(e)
     global timer
-    timer = threading.Timer(t, fun_timer)
+    timer = threading.Timer(t, subtimer)
     timer.start()
 
 
 if __name__ == '__main__':
-    t = int(config.Period)
-    timer = threading.Timer(1, fun_timer)
-    timer.start()
+    if config.Switch == "On":
+        print("定时器已启动..")
+        t = int(config.Period)
+        timer = threading.Timer(1, subtimer)
+        timer.start()
+    else:
+        print("定时器已关闭,请在config中配置为On")
+        sys.exit()
